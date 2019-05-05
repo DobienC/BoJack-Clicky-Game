e53f58c8e801d79a1fd7b9783f76181fe47201c0
DobienC <dobien_c@yahoo.com>
2019-05-04 20:00:14 -0700

    Updates

---
+++ precache-manifest.3423fd5737f86f20c3f194476711a11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f0fddb165a9c54ff94a6fe79cf3e72",
+    "url": "/BoJack-Clicky-Game/index.html"
+  },
+  {
+    "revision": "d7977b36c2da8f2c91c5",
+    "url": "/BoJack-Clicky-Game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/BoJack-Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d7977b36c2da8f2c91c5",
+    "url": "/BoJack-Clicky-Game/static/js/main.80d498a5.chunk.js"
+  },
+  {
+    "revision": "87f4be799b2182fa052f",
+    "url": "/BoJack-Clicky-Game/static/js/runtime~main.5702f935.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BoJack-Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
